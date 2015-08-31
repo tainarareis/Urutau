f26fe8dff72a4af445a5f11a4f349a237f88eb55
Ricardo Canela <ricardo_canela123@hotmail.com>
2015-08-31 20:36:45 +0000

    Created test folder, welcomeUser and welcomeAdministrator views
    Signed-off-by: Ricardo Canela <ricardo_canela123@hotmail.com>
    pwener <phwener@gmail.com>

--- src/main/java/com/modesteam/urutau/controller/UserController.java
+++ src/main/java/com/modesteam/urutau/controller/UserController.java
@@ -7,6 +7,7 @@ import com.modesteam.urutau.controller.dao.UserDAO;
 import com.modesteam.urutau.controller.model.Administrator;
 import com.modesteam.urutau.controller.model.User;
 
+import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Result;
 
 /**
@@ -24,16 +25,27 @@ public class UserController {
 	 * Create new user. If there is no user, create
 	 * the first administrator
 	 */
+	@Post
 	public void register(User user) {
 		// If no account registered, create the first administrator
 		if(!systemDAO.existAdministrator()) {
 			systemDAO.createFirstAdministrator();
+			result.redirectTo(this).welcomeAdministrator();
 		} else {
 			// Create new user
 			userDAO.create(user);
+			result.redirectTo(this).welcomeUser();
 		}
 	}
 	
+	//
+	public void welcomeAdministrator() {
+		
+	}
+	
+	public void welcomeUser() {
+		
+	}
 	
 
 }
---
+++ src/test/java/com/modesteam/urutau/controller/UserControllerTest.java
@@ -0,0 +1,14 @@
+package com.modesteam.urutau.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
