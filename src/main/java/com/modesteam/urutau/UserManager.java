5bec7c652ff87e775aa6b67b2a72c8a3afbfa3af
pwener <phwener@gmail.com>
2015-09-05 01:50:53 -0300

    Adding UserManager

---
+++ src/main/java/com/modesteam/urutau/UserManager.java
@@ -0,0 +1,41 @@
+package com.modesteam.urutau;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+import com.modesteam.urutau.controller.model.User;
+
+@SessionScoped
+@Named("manager")
+public class UserManager implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private User userLogged;
+	
+	/**
+	 * Saves user in session
+	 * 
+	 * @param user to be save in session
+	 */
+	public void login(User user){
+		setUserLogged(user);
+	}
+	
+	/**
+	 * Destroy userLogged
+	 */
+	public void logout(){
+		setUserLogged(null);
+	}
+
+	public User getUserLogged() {
+		return userLogged;
+	}
+
+	public void setUserLogged(User userLogged) {
+		this.userLogged = userLogged;
+	}
+}
