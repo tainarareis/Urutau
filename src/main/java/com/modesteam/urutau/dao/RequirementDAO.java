7d45318cfb0147d51cfa5b2ac6acee25ee1254aa
Rafael Akiyoshi <rafah94@hotmail.com>
2015-09-24 23:21:04 +0000

    created load all requirements dao

--- src/main/java/com/modesteam/urutau/dao/RequirementDAO.java
+++ src/main/java/com/modesteam/urutau/dao/RequirementDAO.java
@@ -1,9 +1,13 @@
 package com.modesteam.urutau.dao;
 
+import java.util.ArrayList;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
+import com.modesteam.urutau.model.Administrator;
 import com.modesteam.urutau.model.Requirement;
 import com.modesteam.urutau.model.UseCase;
 import com.modesteam.urutau.model.UserHistory;
@@ -22,6 +26,14 @@ public class RequirementDAO {
 	}
 	public void saveUseCase(UseCase useCase) {
 		manager.persist(useCase);
+	}
+	public ArrayList<Requirement> loadAll() {
+		String sql = "SELECT r FROM Requirement r";
+		Query query = manager.createQuery(sql);
+		ArrayList<Requirement> requirements;
+		requirements =(ArrayList<Requirement>) query.getResultList();
+		return requirements;
+		
 	}
 
 	
