52e397967c7cca0688cf0d13219b88eb30ba5bde
Rafael Akiyoshi <rafah94@hotmail.com>
2015-11-13 10:32:34 -0200

    Create some methods of project controller(MODEL NECESSARY)

--- src/main/java/com/modesteam/urutau/controller/ProjectController.java
+++ src/main/java/com/modesteam/urutau/controller/ProjectController.java
@@ -1,14 +1,19 @@
 package com.modesteam.urutau.controller;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.modesteam.urutau.UserManager;
+import com.modesteam.urutau.model.User;
 import com.modesteam.urutau.service.RequirementService;
 
 import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.validator.Validator;
 
@@ -38,6 +43,42 @@ public class ProjectController {
 		this.userSession = userSession;
 		this.requirementService = requirementService;
 		this.validator = validator; 
+	}
+	
+	@Post
+	public void createProject(Projetc project){
+		logger.info("Project will be persisted: " + project.getTitle());
+		
+		Date currentDate = new Date();
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(currentDate);
+		
+		project.setDateOfCreation(calendar);
+		
+		User logged = userSession.getUserLogged();
+		project.setAuthor(logged);
+		
+		logger.info("Requesting for project service");
+		projectService.save(project);
+		
+		result.redirectTo(this).showCreationResult(requirement.getId());
+	}
+		
+	}
+	
+	@Post
+	public void deleteProject(){
+		
+	}
+	
+	@Post
+	public void listProjects(){
+		
+	}
+	
+	@Post
+	public void detailProject(){
+		
 	}
 
 }
