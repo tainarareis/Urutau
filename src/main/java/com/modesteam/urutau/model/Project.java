5c80d4c6105dc3defe52adc646de7c67402b99fe
Rafael Akiyoshi <rafah94@hotmail.com>
2015-11-20 00:42:25 -0200

    Create project Model

---
+++ src/main/java/com/modesteam/urutau/model/Project.java
@@ -0,0 +1,41 @@
+package com.modesteam.urutau.model;
+
+import java.util.Calendar;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+
+@Entity
+public class Project {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+
+	private String title;
+	private String description;
+	
+	@ManyToOne
+	@JoinColumn(name="user_id")
+	private User author;
+
+	/* Artifact can be delegated to one or more persons */
+	@ManyToMany
+	@JoinTable(name = "Project_Delegates", joinColumns = @JoinColumn(name = "project_id"), inverseJoinColumns = @JoinColumn(name = "user_id"))
+	private List<User> members;
+
+	/* Should be generate automatically */
+	private Calendar dateOfCreation;
+	
+	
+
+}
