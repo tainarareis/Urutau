8897fcc9df6829bb7d67a0bc7dbb574ad8f865de
pwener <phwener@gmail.com>
2016-04-03 04:46:12 -0300

    Finishing heroku settings

--- src/main/java/com/modesteam/urutau/model/User.java
+++ src/main/java/com/modesteam/urutau/model/User.java
@@ -10,11 +10,13 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
+import javax.persistence.TableGenerator;
 import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -30,11 +32,12 @@ import com.modesteam.urutau.model.system.setting.UserSettingContext;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "userType", discriminatorType = DiscriminatorType.INTEGER)
+@TableGenerator(name="USER_TABLE_ID", initialValue = 0, allocationSize = 1)
 @DiscriminatorValue("1")
 public class User {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.TABLE, generator = "USER_TABLE_ID")
 	private Long userID;
 	
 	@NotNull
