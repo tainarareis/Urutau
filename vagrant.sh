dd64730d2dfdbb72ad65e90ac74aa6371d89df00
Izabela Cardoso <cizabelacristina@gmail.com>
2016-07-11 01:04:15 -0300

    Adding command to vagrant.sh

--- vagrant.sh
+++ vagrant.sh
@@ -6,4 +6,4 @@ echo -e "\nInstalling Vagrant\n"
 sudo apt-get install vagrant
 
 echo -e "\nImage Box (Ubuntu 12.04 64-bit)\n"
-sudo vagrant box add hashicorp/precise64
+sudo vagrant box add precise64 http://files.vagrantup.com/precise64.box
