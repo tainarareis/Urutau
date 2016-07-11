f242ee1f45cd353125a3c712619084f8ebc96b17
Izabela Cardoso <cizabelacristina@gmail.com>
2016-07-11 00:52:30 -0300

    Fixing vagrant.sh

--- vagrant.sh
+++ vagrant.sh
@@ -4,3 +4,6 @@ sudo apt-get install virtualbox
 
 echo -e "\nInstalling Vagrant\n"
 sudo apt-get install vagrant
+
+echo -e "\nInitializing the current directory to be a Vagrant environmen\n"
+sudo vagrant init hashicorp/precise64
